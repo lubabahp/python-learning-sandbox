dc05bf805a31d65dae9a0b6c51a72fcba5f37ec9
Lubabah Pervaj <lubabah.p@outlook.com>
2025-08-26 22:30:49 +0100

    Update if else statements and inputs.py
    fixed age input bug, polished comments, added list.

--- if else statements and inputs.py
+++ if else statements and inputs.py
@@ -7,9 +7,10 @@ if name == "Lubabah":
 else:
     print("Hello there, " + name + "!")
 
-age = input("How old are you?")
 
-if age == 18:
+birthday = input("When is your birthday?")
+
+if birthday == "19th Jan":
   print("cool!")
 else:
   print("nice.")
@@ -18,31 +19,31 @@ else:
 number = input("Enter your favorite number: ")
 number = int(number)  # converting string to integer
 
-if number > 10: # ">" acts like "more than or equal to"
+if number > 10: # ">" is "greater than", ">=" is "greater than or equal to"
     print("that's a big number!")
 else:
     print("that's a small number!")
 
 
 age = input("What is your age?")
-age = int(number) # again, converting string to integer
+age = int(age) # again, converting string to integer
 
-if age > 18:
+if int(age) > 18: # must use int(age) and not just age on its own if you want it to stay an integer, because inputs are always a string. 
   print("adulthood core!")
 else:
   print("you are too young!")
 
 
-answer = input("Do you like Python?").lower()  # converts input to lowercase
+answer = input("Do you like Python?").lower()  # .lower() means it doesn't matter which case the user inputs in, for example python will process "Yes", "YES", etc, the same
 # so "answer" is a variable. a variable can have user input.
 
-if answer == "yes":
+if answer in ["yes", "ye", "yh", "yeah", "yup"]: # square brackets are used for lists
     print("same!")
 else:
     print("hmm, maybe give it a chance!")
 
 
-favourite_colour = input("what is your favourite colour?")   # python thinks "favourite colour" is two words, so use the underscore.
+favourite_colour = input("what is your favourite colour?")   # python thinks "favourite colour" is two words/variables, so use the underscore.
 
 if favourite_colour == "red":
   print("that's bold!")
@@ -59,11 +60,12 @@ months = float(months) # convert months to a floating point number
 if months < 1: # must remember to put the colon
     print("that's REALLY close, damn!")
     
-elif 1 <= months <= 3: # months are now between 1 and 3
+elif 1 <= months <= 3: # months are now between 1 and 3, including 1 and 3 themselves. 
     # must use elif instead of else, because "else" cannot take a condition, it is simply for anything not covered by "if" and "elif" statements. 
     print("that's close, damn!")
 
 else:
     print("ok :)")
+
 
 
