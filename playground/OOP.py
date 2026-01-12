cb16766a5760c2e559aaa3a869637cf46bb40c5c
Lubabah Pervaj <lubabah.p@outlook.com>
2026-01-12 23:05:01 +0000

    Add JJK class with character attributes
    Implemented a basic class structure for JJK with attributes.

---
+++ playground/OOP.py
@@ -0,0 +1,9 @@
+class JJK:
+  def __init__(self, character_name, age, gender):
+    self.character_name = character character_name
+    self.age = age
+    self.gender = gender
+    self.jujutsu_sorcerer = True
+    # lol ig i am getting the idea and use of it now
+
+# i was just playing around with OOP after i understoof it from the library management tutorial thing
